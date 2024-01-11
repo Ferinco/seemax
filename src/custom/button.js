216ab5c153b3b6190e64766b8cfc2a333349bef4
Ferinco <emmanuelferrum003@gmail.com>
2024-01-11 19:39:44 +0100

    custom buttons setup

---
+++ src/custom/button.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+background-color: ${(props)=> (props.red && "blue") || (props.transparent && "transparent") };
+color: ${(props)=> (props.red && "white") || (props.transparent && "#f1f1f1") };
+border: ${(props)=> (props.red && "1px solid red") || (props.transparent && "1px solid #f1f1f1") };
+
+
+`
