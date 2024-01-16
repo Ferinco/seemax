edd7c90ea1da0784846e21df1ff8330b5055623f
Ferinco <emmanuelferrum003@gmail.com>
2024-01-16 10:58:16 +0100

    trending movies div

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { config } from "../config/config";
+
 const mainApi = axios.create({
   baseURL: `https://api.themoviedb.org/3`,
 });
@@ -9,4 +10,4 @@ export const fetchPopularMovies = () =>
 export const fetchMovieImg = (pathName) =>
   `https://image.tmdb.org/t/p/original/${pathName}`;
   export const fetchTrendingMovies = () =>
-  api.get(`/trending/movie/day?api_key=${config.apikey}&language=en-US`);
+  mainApi.get(`/trending/movie/day?api_key=${config.apikey}&language=en-US`);
--- src/custom/sidebar.jsx
+++ src/custom/sidebar.jsx
@@ -1,18 +1,60 @@
 import styled from "styled-components";
 import { useAppContext } from "../contexts/context";
-
+import { fetchMovieImg, fetchTrendingMovies } from "../api/axios";
+import { useEffect, useState } from "react";
 export default function Sidebar() {
   const { isSidebarOpen, setIsSidebarOpen } = useAppContext();
-  console.log(isSidebarOpen);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  async function getMovies() {
+    try {
+      const response = await fetchTrendingMovies();
+      setMovies(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Container
-      className={`h-screen w-3/12 hidden lg:flex flex-col ${isSidebarOpen ? "opened fixed" : "closed"}`}
+      className={`h-screen w-3/12 hidden lg:flex flex-col p-4 gap-2 ${
+        isSidebarOpen ? "opened fixed" : "closed"
+      }`}
     >
-      <button onClick={()=>{setIsSidebarOpen(false)}} className="w-fit">close</button>
-     
+      <div className="flex flex-row justify-end ">
+        <button
+          onClick={() => {
+            setIsSidebarOpen(false);
+          }}
+          className="w-fit p-0 bg-transparent "
+        >
+          &larr;
+        </button>
+      </div>
+      <div className="field rounded-3xl">
+        <input
+          placeholder="search movies"
+          className="px-4 py-3 w-full rounded-3xl"
+        />
+      </div>
+      <div className="h-[500px] border rounded-[30px] trending-div overflow-auto flex flex-col gap-3 px-7 py-6">
+        <p className="fixed">Trending</p>
+        <div className="flex flex-col gap-3 mt-9">
+        {
+          movies.map((movie)=>(
+            <div className="image h-48 rounded-[20px]">
+              <img src={fetchMovieImg(movie.backdrop_path)} className="w-full h-full rounded-[20px] object-cover object-center"/>
+            </div>
+          ))
+        }
+        </div>
+      </div>
     </Container>
   );
 }
 const Container = styled.div`
-  border: 1px solid white;
+  border-right: 1px solid white;
 `;
