5f4c76d6b4b2c17bc40cf501b0b8eda10fdf3beb
Ferinco <emmanuelferrum003@gmail.com>
2024-01-19 12:05:58 +0100

    populated the top casts page

--- src/layouts/components/topCrew.jsx
+++ src/layouts/components/topCrew.jsx
@@ -1,7 +1,54 @@
-export default function TopCrew(){
-    return(
-        <div>
-            
+import { useEffect, useState } from "react";
+import { fetchCrew, fetchMovieImg } from "../../utils/api/axios";
+
+export default function TopCrew() {
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    getCasts();
+  }, []);
+  async function getCasts() {
+    try {
+      const response = await fetchCrew();
+      console.log(response.data.results);
+      setPeople(response.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div className="flex flex-col">
+      <div className="flex justify-normal">
+        <h4>Top Cast</h4>
+      </div>
+      {
+        <div className="flex flex-row justify-between h-[500px] gap-4 w-fit">
+          {people?.slice(0, 5).map((person, index) => (
+            <div
+              key={index}
+              className={`flex flex-col h-80 w-60 justify-between items-center relative ${
+                index % 2 === 0 ? "mt-8" : ""
+              }`}
+            >
+              <p className="index z-10">{index + 1}</p>
+              <h6 className="z-10">{person?.name}</h6>
+              <img
+                className="w-48 h-64 object-cover object-center mb-[-50px] z-10"
+                src={fetchMovieImg(person.profile_path)}
+                alt={`Profile of ${person.name}`}
+              />
+              <div className="absolute w-full h-full flex flex-col">
+                <img
+                  className="w-full h-full object-cover object-center"
+                  src={fetchMovieImg(person.profile_path)}
+                />
+                <div className="absolute w-full h-full backdrop-blur-lg bg-black/85 opacity-90">
+
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-    )
+      }
+    </div>
+  );
 }
