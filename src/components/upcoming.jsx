db0503e084a470c192a9ff366cb591e1dee12e7a
Ferinco <emmanuelferrum003@gmail.com>
2024-01-19 02:13:26 +0100

    new releases page design

--- src/components/upcoming.jsx
+++ src/components/upcoming.jsx
@@ -1,10 +1,38 @@
-export default function UpcomingMovies(){
+import { useEffect, useState } from "react";
+import { fetchMovieImg, getUpcomingMovies } from "../utils/api/axios";
+import { Link } from "react-router-dom";
 
+export default function UpcomingMovies() {
+    const [movies, setMovies] = useState([])
+    useEffect(()=>{
+        getMovies()
+    }, [])
+  async function getMovies() {
+    try {
+      const response = await getUpcomingMovies();
+      setMovies(response.data.results);
+      console.log(response.data.results)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-    
-    retrun(
-        <div>
+  return (
+  <div className="flex flex-col gap-6">
+    <div className="header text-start flex flex-row justify-between">
+        <h4>New Releases</h4>
+        <Link>view more</Link>
+    </div>
+    <div className=" grid grid-cols-2 gap-4">
+        {
+            movies?.slice(0,4).map((movie)=>(
+                <div className="image  h-64 rounded-[30px]">
+                    <img src={fetchMovieImg(movie.backdrop_path)} className="h-full w-full object-cover object-top rounded-[30px]"/>
+                </div>
+            ))
+        }
+    </div>
 
-        </div>
-    )
+  </div>
+  )
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,12 +2,14 @@ import { useAppContext } from "../../contexts/context";
 import LandingHero from "../../components/landingHero";
 import styled from "styled-components";
 import FilterMovies from "../../components/filterMovies";
+import UpcomingMovies from "../../components/upcoming";
 export default function HomePage() {
   const { isSidebarOpen } = useAppContext();
   return (
     <Container className="w-full px-7 flex flex-col gap-10" >
       <LandingHero />
       <FilterMovies/>
+      <UpcomingMovies/>
     </Container>
   );
 }
